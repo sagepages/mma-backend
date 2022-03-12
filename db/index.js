cdcdaf7708edcd9ba59cff1866d582ea4e41f86b
sagepages <anthonypages009@gmail.com>
2022-03-12 00:28:17 -0500

    db endpoint changed

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const pool = new Pool({
-    connectionString: process.env.HEROKU_DB,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
       }
